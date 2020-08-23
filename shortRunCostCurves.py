36144b6fd047daa60f86ff344a5d81dd913b9053
ddau-stack <dudau0924222@gmail.com>
2020-08-23 09:54:12 -0400

    3 basic cost curves are computed and plotted on a graph

--- shortRunCostCurves.py
+++ shortRunCostCurves.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plot
+
 def createVariableCosts(quantity):
 	array = []
 	for i in range(0, quantity):
@@ -27,14 +29,31 @@ def createAvgFixedCosts(fixed_cost, quantity):
 		array.append(float(fixed_cost)/(i+1))
 	return array
 
+def createQuantities(quantity):
+	array = []
+	for i in range(0, quantity):
+		array.append(int(i+1))
+	return array
+
+# get inputs
 total_quantity = int(input("What is the total quantity of output? "))
 fixed_cost = float(input("Enter the total fixed costs: "))
+
+# computes the costs
 variable_costs = createVariableCosts(total_quantity)
 total_costs = createTotalCosts(fixed_cost, variable_costs, total_quantity)
 avg_variable_costs = createAvgCost(variable_costs, total_quantity)
 avg_total_costs = createAvgCost(total_costs, total_quantity)
 avg_fixed_costs = createAvgFixedCosts(fixed_cost, total_quantity)
-printCost(variable_costs)
-printCost(total_costs)
-printCost(avg_total_costs)
-printCost(avg_fixed_costs)
+
+quantity = createQuantities(total_quantity)
+
+# creates the graph
+plot.title("Short Run Cost Curves")
+plot.xlabel("Quantity")
+plot.ylabel("Price")
+plot.plot(quantity, avg_variable_costs, label = "Average Variable Cost")
+plot.plot(quantity, avg_fixed_costs, label = "Average Fixed Cost")
+plot.plot(quantity, avg_total_costs, label = "Average Total Cost")
+plot.legend()
+plot.show()
