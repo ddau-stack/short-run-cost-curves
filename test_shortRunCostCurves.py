66bac9841c794e6e780d08b18a08c1da7ddf309b
ddau-stack <dudau0924222@gmail.com>
2020-08-30 13:13:17 -0400

    create unit tests for the readTotalQuantity and readPricePerUnit functions

--- test_shortRunCostCurves.py
+++ test_shortRunCostCurves.py
@@ -5,6 +5,18 @@ import shortRunCostCurves
 
 class TestShortRunCostCurves(unittest.TestCase):
 	@mock.patch('shortRunCostCurves.input', create=True)
+	def testReadTotalQuantity(self, mocked_input):
+		mocked_input.side_effect = [0, -3, -12, 7]
+		result = shortRunCostCurves.readTotalQuantity()
+		self.assertEqual(result, 7)
+
+	@mock.patch('shortRunCostCurves.input', create=True)
+	def testReadPricePerUnit(self, mocked_input):
+		mocked_input.side_effect = [-40, -33.0, 0, 0.0, 12]
+		result = shortRunCostCurves.readPricePerUnit()
+		self.assertEqual(result, 12.0)
+
+	@mock.patch('shortRunCostCurves.input', create=True)
 	def test_createVariableCosts(self, mocked_input):
 		mocked_input.side_effect = [1.0, 2.0, 3.0, 4.0]
 		result = shortRunCostCurves.createVariableCosts(4)
