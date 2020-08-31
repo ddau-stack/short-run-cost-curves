9d0b848f889a5b3f1b96de15cb33605dd45da112
ddau-stack <dudau0924222@gmail.com>
2020-08-31 13:21:51 -0400

    add unit test for readFixedCost

--- test_shortRunCostCurves.py
+++ test_shortRunCostCurves.py
@@ -17,10 +17,16 @@ class TestShortRunCostCurves(unittest.TestCase):
 		self.assertEqual(result, 12.0)
 
 	@mock.patch('shortRunCostCurves.input', create=True)
-	def test_createVariableCosts(self, mocked_input):
-		mocked_input.side_effect = [1.0, 2.0, 3.0, 4.0]
-		result = shortRunCostCurves.createVariableCosts(4)
-		self.assertEqual(result, [0.0, 1.0, 2.0, 3.0, 4.0])
+	def testReadFixedCost(self, mocked_input):
+		mocked_input.side_effect = [0.0, -50, 30]
+		result = shortRunCostCurves.readFixedCost()
+		self.assertEqual(result, 30.0)
+
+#	@mock.patch('shortRunCostCurves.input', create=True)
+#	def testCreateVariableCosts(self, mocked_input):
+#		mocked_input.side_effect = [1.0, 2.0, 3.0, 4.0]
+#		result = shortRunCostCurves.createVariableCosts(4)
+#		self.assertEqual(result, [0.0, 1.0, 2.0, 3.0, 4.0])
 
 if __name__ == '__main__':
 	unittest.main()
