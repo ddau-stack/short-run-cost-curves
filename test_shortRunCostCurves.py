7731dc71e1b88aabc11035ba47b33535fb449771
ddau-stack <dudau0924222@gmail.com>
2020-08-31 13:31:32 -0400

    add unit test for createTotalCosts

--- test_shortRunCostCurves.py
+++ test_shortRunCostCurves.py
@@ -22,11 +22,18 @@ class TestShortRunCostCurves(unittest.TestCase):
 		result = shortRunCostCurves.readFixedCost()
 		self.assertEqual(result, 30.0)
 
-#	@mock.patch('shortRunCostCurves.input', create=True)
-#	def testCreateVariableCosts(self, mocked_input):
-#		mocked_input.side_effect = [1.0, 2.0, 3.0, 4.0]
-#		result = shortRunCostCurves.createVariableCosts(4)
-#		self.assertEqual(result, [0.0, 1.0, 2.0, 3.0, 4.0])
+	@mock.patch('shortRunCostCurves.input', create=True)
+	def testCreateVariableCosts(self, mocked_input):
+		mocked_input.side_effect = [1.0, 2.0, 3.0, 4.0]
+		result = shortRunCostCurves.createVariableCosts(4)
+		self.assertEqual(result, [0.0, 1.0, 2.0, 3.0, 4.0])
+
+	def testCreateTotalCosts(self):
+		testQuantity = 5
+		testVariableCosts = [70, 90, 130, 170, 190]
+		testFixedCost = 40
+		result = shortRunCostCurves.createTotalCosts(testFixedCost, testVariableCosts, testQuantity)
+		self.assertEqual(result, [110.0, 130.0, 170.0, 210.0, 230.0])
 
 if __name__ == '__main__':
 	unittest.main()
