1ab77505d3447c1e5c7990d29d82c86f1ab9718e
ddau-stack <dudau0924222@gmail.com>
2020-09-01 13:01:36 -0400

    add test strings for testing the readTotalQuantity, readPricePerUnit, readFixedCost and createVariableCosts functions

--- test_shortRunCostCurves.py
+++ test_shortRunCostCurves.py
@@ -6,27 +6,27 @@ import shortRunCostCurves
 class TestShortRunCostCurves(unittest.TestCase):
 	@mock.patch('shortRunCostCurves.input', create=True)
 	def testReadTotalQuantity(self, mocked_input):
-		mocked_input.side_effect = [0, -3, -12, 7]
+		mocked_input.side_effect = [0, -3, -12, "test", "useless string", " test2", 7]
 		result = shortRunCostCurves.readTotalQuantity()
 		self.assertEqual(result, 7)
 
 	@mock.patch('shortRunCostCurves.input', create=True)
 	def testReadPricePerUnit(self, mocked_input):
-		mocked_input.side_effect = [-40, -33.0, 0, 0.0, 12]
+		mocked_input.side_effect = [-40, -33.0, "test", "useless string", " test2", 0, 0.0, 12]
 		result = shortRunCostCurves.readPricePerUnit()
 		self.assertEqual(result, 12.0)
 
 	@mock.patch('shortRunCostCurves.input', create=True)
 	def testReadFixedCost(self, mocked_input):
-		mocked_input.side_effect = [0.0, -50, 30]
+		mocked_input.side_effect = [0.0, -50, "test", "useless string", " test2", 30]
 		result = shortRunCostCurves.readFixedCost()
 		self.assertEqual(result, 30.0)
 
 	@mock.patch('shortRunCostCurves.input', create=True)
 	def testCreateVariableCosts(self, mocked_input):
-		mocked_input.side_effect = [1.0, 2.0, 3.0, 4.0]
+		mocked_input.side_effect = [10, "useless string", 30, "test", 50, " test2", 70]
 		result = shortRunCostCurves.createVariableCosts(4)
-		self.assertEqual(result, [0.0, 1.0, 2.0, 3.0, 4.0])
+		self.assertEqual(result, [0.0, 10.0, 30.0, 50.0, 70.0])
 
 	def testCreateTotalCosts(self):
 		testQuantity = 5
